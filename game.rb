041987f6125a4eaadc4dac2bd7f58dc706e599e4
husna <hpm25@georgetown.edu>
2022-09-24 15:57:35 -0700

    add game class

---
+++ game.rb
@@ -0,0 +1,27 @@
+require './display'
+
+class Game
+  include Display
+
+  def play
+    puts game_intro
+    puts game_instructions
+    puts game_clues
+    puts game_mode
+    mode = game_mode_selection
+    code_maker if mode == '1'
+    code_breaker if mode == '2'
+  end
+
+  def game_mode_selection
+    selection = gets.chomp
+    return selection if selection.match?(/^[1-2]$/)
+
+    puts "\nInvalid selection."
+    game_mode_selection
+  end
+
+  def code_maker; end
+
+  def code_breaker; end
+end
